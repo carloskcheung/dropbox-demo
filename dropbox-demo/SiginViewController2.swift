3d2c5466751f8eccc44f7ebccefd18ceaad7fb94
Carlos Cheung <cc653@georgetown.edu>
2015-05-09 19:52:06 -0700

    navigation changes

---
+++ dropbox-demo/SiginViewController2.swift
@@ -0,0 +1,39 @@
+//
+//  SiginViewController2.swift
+//  dropbox-demo
+//
+//  Created by Carlos Cheung on 5/9/15.
+//  Copyright (c) 2015 carloscheung. All rights reserved.
+//
+
+import UIKit
+
+class SiginViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func siginBackButton(sender: AnyObject) {
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
